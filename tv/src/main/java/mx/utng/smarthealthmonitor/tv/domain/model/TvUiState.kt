931deec96634528f19795108ff8b41e3c41a4714
carolross1 <c4rolrios347@gmail.com>
2026-07-17 13:38:39 -0600

    feat(tv/mqtt): add MqttTvSubscriber - subscribes to TV topic and updates TvUiState reactively

--- tv/src/main/java/mx/utng/smarthealthmonitor/tv/domain/model/TvUiState.kt
+++ tv/src/main/java/mx/utng/smarthealthmonitor/tv/domain/model/TvUiState.kt
@@ -5,6 +5,8 @@ import mx.utng.smarthealthmonitor.data.db.LecturaFC
 data class TvUiState(
     val lecturas: List<LecturaFC> = emptyList(),
     val fcActual: Int = 0,
+    val fcEstado: String = "",
+    val ultimaHora: String = "",
     val isLoading: Boolean = true,
     val error: String? = null,
 )
